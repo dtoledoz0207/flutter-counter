b6ac31539c0676f8f7d9dfc6e5060d5d641c3d9a
David Toledo <dtoledoz0207@gmail.com>
2020-08-04 10:48:14 -0500

    hello-world

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
-
+import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text('Hello world'),
+      ),
+    );
+  }
+
 }
