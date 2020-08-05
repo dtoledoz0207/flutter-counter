4cecbde630f2e903362bdb8867579cd0ab188790
David Toledo <dtoledoz0207@gmail.com>
2020-08-05 10:29:34 -0500

    create-appFile-to-admin-logic

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text('Hello world!'),
+      ),
+    );
+  }
+
+}
