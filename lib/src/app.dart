15db1a25b857e09d4c2d8fa1292c5f15ade5c8a1
David Toledo <dtoledoz0207@gmail.com>
2020-08-05 10:59:37 -0500

    create-homePage-and-use-Scaffold-widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:contador/src/pages/home_page.dart';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text('Hello world!'),
-      ),
+      home: HomePage()
     );
   }
 
