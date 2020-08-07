21848c019dfd2d335a4dbbf2f89f8d36670a475e
David Toledo <dtoledoz0207@gmail.com>
2020-08-07 10:49:33 -0500

    create-counterPage-StatefulWidget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:contador/src/pages/home_page.dart';
+//import 'package:contador/src/pages/home_page.dart';
+import 'package:contador/src/pages/counter.page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      //home: HomePage()
+      debugShowCheckedModeBanner: false,
+      home: CounterPage(),
     );
   }
 
--- lib/src/pages/counter.page.dart
+++ lib/src/pages/counter.page.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
 
-  final styleText = new TextStyle(fontSize: 20.5, fontWeight: FontWeight.bold);
+class CounterPage extends StatefulWidget {
+  createState() => new _CounterPageState();
+}
 
-  final int counter = 10;
+class _CounterPageState extends State<CounterPage> {
+
+  final _styleText = new TextStyle(fontSize: 20.5, fontWeight: FontWeight.bold);
+
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Titulo'),
+        title: Text('Stateful'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Number of clicks:', style: styleText),
-            Text('$counter', style: styleText)
+            Text('Number of taps:', style: _styleText),
+            Text('$_counter', style: _styleText)
           ],
         )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print('Hello world');
+          setState(() {
+            _counter++;
+          });
         },
         child: Icon(Icons.add_circle),
       ),
